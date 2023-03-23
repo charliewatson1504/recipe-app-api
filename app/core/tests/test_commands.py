53ea3202cc372c71462fc3fa2184a320b218d413
Charlie Watson <charliewatson87@gmail.com>
2023-03-23 11:50:04 +0000

    Configured docker compose and checks to wait for db

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,7 @@
 """
 Test custom Django management commands.
 """
-from dataclasses import dataclass
+from dataclasses import dataclass  # noqa: F401
 from unittest.mock import patch
 
 from psycopg2 import OperationalError as Psycopg2Error
@@ -21,7 +21,7 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
@@ -32,4 +32,4 @@ class CommandTests(SimpleTestCase):
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_with(database=['default'])
+        patched_check.assert_called_with(databases=['default'])
